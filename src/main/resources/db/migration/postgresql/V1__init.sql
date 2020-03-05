a48b5b48f88255d0877a387f30c1a7e2975336c5
KarenF <karenf@outlook.com>
2020-03-05 12:48:33 -0300

    add db

---
+++ src/main/resources/db/migration/postgresql/V1__init.sql
@@ -0,0 +1,15 @@
+create table cidades(
+id serial,
+nome_cidade varchar(50),
+estado varchar(50),
+primary key(id));
+
+create table cliente(
+id_cliente serial,
+nome_cliente varchar(50),
+genero varchar(50),
+data_nascimento varchar(50),
+idade integer,
+cidades integer,
+primary key (id_cliente),
+foreign key(cidades) references cidades(id));
