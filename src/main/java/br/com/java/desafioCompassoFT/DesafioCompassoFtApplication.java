05c44e4c5571aee31ccedad365e74b8bf37aba6d
KarenF <karenf@outlook.com>
2020-03-05 10:54:52 -0300

    add project

---
+++ src/main/java/br/com/java/desafioCompassoFT/DesafioCompassoFtApplication.java
@@ -0,0 +1,13 @@
+package br.com.java.desafioCompassoFT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioCompassoFtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioCompassoFtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/java/desafioCompassoFT/DesafioCompassoFtApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.java.desafioCompassoFT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioCompassoFtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
