57f3075a54a80a38bd7bc17761b1726f1fb246a4
KarenF <karenf@outlook.com>
2020-03-05 14:27:35 -0300

    update entity tables

--- src/main/java/br/com/java/desafioCompassoFT/entity/Cidades.java
+++ src/main/java/br/com/java/desafioCompassoFT/entity/Cidades.java
@@ -3,9 +3,29 @@ package br.com.java.desafioCompassoFT.entity;
 import java.io.Serializable;
 import java.util.List;
 
-import lombok.Data;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
 
+import org.hibernate.annotations.DynamicUpdate;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "cidades")
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@DynamicUpdate
 public class Cidades implements Serializable {
 
 	/**
@@ -13,9 +33,17 @@ public class Cidades implements Serializable {
 	 */
 	private static final long serialVersionUID = -5076998331643312343L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id", updatable = false, nullable = false)
 	private Long id;
+	@NotBlank
+	@Column(nullable = false)
 	private String nomeCidade;
+	@NotBlank
+	@Column(nullable = false)
 	private String estado;
-	
+
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "cidades", cascade = CascadeType.ALL)
 	private List<Cliente> cliente;
 }
--- src/main/java/br/com/java/desafioCompassoFT/entity/Cliente.java
+++ src/main/java/br/com/java/desafioCompassoFT/entity/Cliente.java
@@ -2,21 +2,54 @@ package br.com.java.desafioCompassoFT.entity;
 
 import java.io.Serializable;
 
-import lombok.Data;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
 
+import org.hibernate.annotations.DynamicUpdate;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "cliente")
 @Data
-public class Cliente implements Serializable{
+@NoArgsConstructor
+@AllArgsConstructor
+@DynamicUpdate
+public class Cliente implements Serializable {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -648906708267780745L;
 
-	private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "idCliente", updatable = false, nullable = false)
+	private Long idCliente;
+	@NotBlank
+	@Column(nullable = false)
 	private String nomeCliente;
+	@NotBlank
+	@Column(nullable = false)
 	private String genero;
+	@NotBlank
+	@Column(nullable = false)
 	private String dataNascimento;
+	@NotBlank
+	@Column(nullable = false)
 	private int idade;
-	
+
+	@JoinColumn(name = "cidades", referencedColumnName = "id", updatable = false, nullable = false)
+	@ManyToOne(fetch = FetchType.LAZY)
 	private Cidades cidades;
 }
