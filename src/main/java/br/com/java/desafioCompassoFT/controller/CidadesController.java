4bedd0909b736738b5e79bc898c82c242fe84957
KarenF <karenf@outlook.com>
2020-03-06 07:36:39 -0300

    add save cidades

--- src/main/java/br/com/java/desafioCompassoFT/controller/CidadesController.java
+++ src/main/java/br/com/java/desafioCompassoFT/controller/CidadesController.java
@@ -1,10 +1,15 @@
 package br.com.java.desafioCompassoFT.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import br.com.java.desafioCompassoFT.entity.Cidades;
 import br.com.java.desafioCompassoFT.service.CidadesService;
 
 @Controller
@@ -13,8 +18,21 @@ public class CidadesController {
 	@Autowired
 	private CidadesService cidadesService;
 	
-	@RequestMapping(value = "/buscar", method = RequestMethod.GET)
+	@RequestMapping(value = "/cadastrarCidades", method = RequestMethod.GET)
 	public String form() {
-		return "index";
+		return "form";
+	}
+	
+	@RequestMapping(value = "/cadastrarCidades", method = RequestMethod.POST)
+	public String form(@Valid Cidades cidades, BindingResult result, RedirectAttributes attributes) {
+
+		if (result.hasErrors()) {
+			attributes.addFlashAttribute("mensagem", "Verifique os campos");
+			return "redirect:/cadastrarCidades";
+		}
+
+		cidadesService.save(cidades);
+		attributes.addFlashAttribute("mensagem", "Cidade adicionada com sucesso");
+		return "redirect:/cadastrarCidades";
 	}
 }
