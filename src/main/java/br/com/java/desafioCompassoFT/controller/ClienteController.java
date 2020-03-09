140863fe78d27d4fecbbc5d21f1c077e39825295
KarenF <karenf@outlook.com>
2020-03-09 09:39:32 -0300

    add update nomeCliente

--- src/main/java/br/com/java/desafioCompassoFT/controller/ClienteController.java
+++ src/main/java/br/com/java/desafioCompassoFT/controller/ClienteController.java
@@ -1,12 +1,15 @@
 package br.com.java.desafioCompassoFT.controller;
 
+import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.java.desafioCompassoFT.entity.Cliente;
@@ -17,12 +20,12 @@ public class ClienteController {
 
 	@Autowired
 	private ClienteService clienteService;
-	
+
 	@RequestMapping(value = "/cadastrarCliente", method = RequestMethod.GET)
 	public String form() {
 		return "cadastrarCliente";
 	}
-	
+
 	@RequestMapping(value = "/cadastrarCliente", method = RequestMethod.POST)
 	public String form(@Valid Cliente cliente, BindingResult result, RedirectAttributes attributes) {
 
@@ -34,5 +37,29 @@ public class ClienteController {
 		clienteService.save(cliente);
 		attributes.addFlashAttribute("mensagem", "Cliente adicionada com sucesso");
 		return "redirect:/cadastrarCliente";
+	}
+
+	@RequestMapping(value = "/atualizarCliente/{idCliente}", method = RequestMethod.GET)
+	public ModelAndView atualizarCliente(@PathVariable(name = "idCliente") Long idCliente) {
+		Cliente cliente = clienteService.findByIdCliente(idCliente);
+		ModelAndView mv = new ModelAndView("atualizarCliente");
+		mv.addObject("cliente", cliente);
+
+		return mv;
+	}
+
+	@Transactional
+	@RequestMapping(value = "/atualizarCliente/{idCliente}", method = RequestMethod.POST)
+	public String atualizarClientePost(@PathVariable("idCliente") Long idCliente, @Valid Cliente cliente,
+			BindingResult result, RedirectAttributes attributes) {
+
+		if (result.hasErrors()) {
+			attributes.addFlashAttribute("mensagem", "Verifique os campos");
+			return "redirect:/atualizarCliente/{idCliente}";
+		}
+
+		clienteService.save(cliente);
+		attributes.addFlashAttribute("mensagem", "Cliente atualizado com sucesso");
+		return "redirect:/";
 	}
 }
